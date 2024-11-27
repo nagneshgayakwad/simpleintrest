1ffa5c1948dc9d9cfbcba57171df667f26c4a0ea
nagnesh gayakwad <119508488+nagneshgayakwad@users.noreply.github.com>
2024-11-27 21:27:31 +0530

    Create simpleintrest.sh

---
+++ simpleintrest.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Enter principal amount: "
+read principal
+echo "Enter rate of interest: "
+read rate
+echo "Enter time period: "
+read time
+interest=$(echo "$principal * $rate * $time / 100" | bc)
+echo "Simple Interest: $interest"
